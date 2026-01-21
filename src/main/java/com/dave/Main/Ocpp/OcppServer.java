0aaf245ecfdf6fb96ea3ba9ab0a84193dfdd6349
Dave <80040393+DaveWasTakn@users.noreply.github.com>
2026-01-21 21:49:35 +0100

    refactor OcppServer to implement SmartLifecycle

--- src/main/java/com/dave/Main/Ocpp/OcppServer.java
+++ src/main/java/com/dave/Main/Ocpp/OcppServer.java
@@ -2,50 +2,76 @@ package com.dave.Main.Ocpp;
 
 import com.dave.Main.Logging.Logger;
 import com.dave.Main.State.State;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Component
-public class OcppServer {
-    private static final Logger LOGGER = Logger.INSTANCE;
+public class OcppServer implements SmartLifecycle {
+    private static final Logger LOGGER = Logger.INSTANCE; // TODO replace with normal logger after implementing the ocpp spec
 
     private final State state;
+    private ServerSocket serverSocket;
+    private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+
+    private boolean isRunning = false;
 
     @Autowired
     public OcppServer(State state) {
         this.state = state;
     }
 
-    @PostConstruct
-    public void init() {
-        new Thread(this::start).start();
+    @Override
+    public void start() {
+        isRunning = true;
+        Thread.startVirtualThread(this::runServer);
     }
 
-
-    private void start() {
+    private void runServer() {
         try (ServerSocket ws = new ServerSocket(1234)) {
+            this.serverSocket = ws;
             LOGGER.print("Waiting for client on port 1234 ...");
 
-            try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-
-                while (!ws.isClosed()) {
-                    Socket socket = ws.accept();
-                    executorService.execute(new OcppSocketHandler(socket, state));
-                }
-
+            while (!ws.isClosed()) {
+                Socket socket = ws.accept();
+                this.executorService.execute(new OcppSocketHandler(socket, state));
             }
 
         } catch (Exception e) {
+            if (!isRunning) { // shutdown
+                return;
+            }
             LOGGER.print(e.getMessage());
-            LOGGER.print("Restarting ...");
-            start();
+            LOGGER.print("Restarting OcppServer ...");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException _) {
+            }
+            this.runServer();
         }
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+        this.executorService.shutdown();
+        if (this.serverSocket != null) {
+            try {
+                this.serverSocket.close();
+            } catch (IOException _) {
+            }
+        }
+    }
+
+    @Override
+    public boolean isRunning() {
+        return this.isRunning;
     }
 
 }
